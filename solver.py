b1b3ea641e1a4a5e151fadd714f9d02d554f38c1
Pavan Mallik <54065955+pavan55523@users.noreply.github.com>
2020-07-06 19:16:36 +0530

    Update solver.py

--- solver.py
+++ solver.py
@@ -16,21 +16,21 @@ def find_empty_location(arr, l):
                 return True
     return False
   
-# Returns a boolean which indicates whether any assigned entry in the specified row matches the given number. 
+#Function to find whether any assigned entry in the specified row matches the given number. 
 def used_in_row(arr, row, num): 
     for i in range(9): 
         if(arr[row][i] == num): 
             return True
     return False
   
-# Returns a boolean which indicates whether any assigned entry in the specified column matches the given number. 
+# Function to find whether any assigned entry in the specified column matches the given number. 
 def used_in_col(arr, col, num): 
     for i in range(9): 
         if(arr[i][col] == num): 
             return True
     return False
   
-# Returns a boolean which indicates whether any assigned entry within the specified 3x3 box matches the given number 
+# Function to find whether any assigned entry within the specified 3x3 box matches the given number 
 def used_in_box(arr, row, col, num): 
     for i in range(3): 
         for j in range(3): 
@@ -39,14 +39,8 @@ def used_in_box(arr, row, col, num):
     return False
   
 def check_location_is_safe(arr, row, col, num): 
-      
-    # Check if 'num' is not already placed in current row, 
-    # current column and current 3x3 box 
     return not used_in_row(arr, row, num) and not used_in_col(arr, col, num) and not used_in_box(arr, row - row%3, col - col%3, num) 
   
-# Takes a partially filled-in grid and attempts to assign values to 
-# all unassigned locations in such a way to meet the requirements 
-# for Sudoku solution (non-duplication across rows, columns, and boxes) 
 def solve_sudoku(arr): 
       
     # 'l' is a list variable that keeps the record of row and col in find_empty_location Function     
@@ -69,7 +63,7 @@ def solve_sudoku(arr):
             # make tentative assignment 
             arr[row][col]= num 
   
-            # return, if success, ya !
+            # return, if success
             if(solve_sudoku(arr)): 
                 return True
   
@@ -78,8 +72,7 @@ def solve_sudoku(arr):
               
     # this triggers backtracking         
     return False 
-  
-# Driver main function to test above functions 
+ 
 if __name__=="__main__":
     ele=[]
       
